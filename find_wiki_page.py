0ef69af08655c545026bd379df53261db3c0871e
Khoroshevskyi <sasha99250@gmail.com>
2021-03-25 02:13:58 +0100

    Complete version 1.1 with logger

--- find_wiki_page.py
+++ find_wiki_page.py
@@ -2,6 +2,7 @@ import pywikibot
 import json
 import requests
 import re
+import logging
 
 """
 Sometimes pywikibot is not working so it's better to write own code in sparql!
@@ -41,11 +42,13 @@ data= ["Cell division ATP-binding protein FtsE Smed_2543","cell division ATP-bin
 
 class FindWikiPage(object):
     def __init__(self, config=None):
+        logging.basicConfig(filename='find_wiki.log', level=logging.DEBUG,
+                            filemode='w+', format='%(asctime)s:%(levelname)s:%(message)s')
+
         print("FindWikiPage initiated...")
+        logging.info("FindWikiPage initiated...")
         self.site = pywikibot.Site("wikidata", "wikidata")
         self.repo = self.site.data_repository()
-        self.gebe_searched_list = []
-
 
     # searching items by expressions by using API
     def search_entities(self, itemtitle):
@@ -62,192 +65,187 @@ class FindWikiPage(object):
         )
 
         if response.status_code != 200:
+            logging.error(f"Error while searching for information, response: {response.status_code}")
             raise Exception("Bad response:(")
-        json_response = response.json()
-        #self.pretty_response(json_response)
-        return json_response
+
+        return response.json()
 
     # just prints pretty
     def pretty_response(self, json_obj):
         print(json.dumps(json_obj, indent=4))
 
-    # Searching pages linked by reference number and returns list of id's (of linked pages)
-    def search_statements(self, item_identifier, reference_nr):
+    # Searching statement_value by property_id and returns list of property_id's
+    def search_statements(self, item_identifier, property_id):
         try:
             item = pywikibot.ItemPage(self.repo, item_identifier)
-
             item_dict = item.get()  # Get the item dictionary
-            # print(item_dict.keys())
-            # for dd in item_dict["sitelinks"]:
-            #    print(dd)
-            # print(item_dict["claims"])
             clm_dict = item_dict["claims"]  # Get the claim dictionary
 
-            clm_dict1 = clm_dict[reference_nr]
-            resp_arr = []
-            for clm in clm_dict1:
+            prop_id_list = []
+            for clm in clm_dict[property_id]:
                 response_dict = clm.toJSON()
-                # pretty_response(response_dict)
-                # print(response_dict)
-                resp_arr.append(response_dict['mainsnak']['datavalue']['value']['numeric-id'])
-            return resp_arr
+                prop_id_list.append(response_dict['mainsnak']['datavalue']['value']['numeric-id'])
+
+            return prop_id_list
+
         except KeyError as err:
-            print(f"There is no: {err}")
+            logging.error(f"There is no: {err} in {item_identifier}")
             return None
 
     # Gets the item dictionary with information: dict_keys(['aliases', 'sitelinks', 'descriptions', 'claims', 'labels'])
-    def get_item_info(self, id):
-        item = pywikibot.ItemPage(self.repo, id)
+    def get_item_info(self, statement_id):
+        item = pywikibot.ItemPage(self.repo, statement_id)
         item_dict = item.get()  # Get the item dictionary
         return item_dict
 
     # Gets the item label (in English)
-    def get_item_label(self, id):
+    def get_item_label(self, statement_id):
         try:
-            item_dict = self.get_item_info(id)
+            item_dict = self.get_item_info(statement_id)
             clm_dict = item_dict["labels"]  # Get the claim dictionary
             item_label = clm_dict.toJSON()['en']['value']
-            # print(item_label)
             return item_label
         except:
-            print(f"{id} No label were found")
+            logging.warning(f"{statement_id} No label were found")
             return ""
 
     # Gets the item description(in English)
-    def get_item_descriptions(self, id):
+    def get_item_descriptions(self, statement_id):
         try:
-            item_dict = self.get_item_info(id)
+            item_dict = self.get_item_info(statement_id)
             clm_dict = item_dict["descriptions"]  # Get the claim dictionary
             item_description = clm_dict.toJSON()['en']['value']
-            # print(item_description)
+
             return item_description
         except:
-            print(f"{id} No description were found")
+            logging.warning(f"{statement_id} No description were found")
             return ""
 
     # Gets the item aliases(in English)
-    def get_item_aliases(self, id):
+    def get_item_aliases(self, statement_id):
         try:
-            item_dict = self.get_item_info(id)
+            item_dict = self.get_item_info(statement_id)
             clm_dict = item_dict["aliases"]  # Get the claim dictionary
-            item_description = clm_dict['en']
-            # print(item_description)
-            return item_description
+            item_aliases = clm_dict['en']
+            return item_aliases
+
         except:
-            print(f"{id} No aliases were found")
+            logging.warning(f"{statement_id} No aliases were found")
             return ""
 
     # receiving id of item where to search, id of statement which we want to search, and value which we are searching
     # returns id if value is found
     def search_by_property(self, id_info, property_id, value):
-        statements = self.search_statements(id_info, property_id)
-        if statements:
-            for output in statements:
-                output1 = 'Q' + str(output)
+        logging.error(f"Searching property value: {value} by property id: {property_id} in {id_info}")
+        statement_id_list = self.search_statements(id_info, property_id)  # searching statement value by property id
+        if statement_id_list:
+            for statement_id in statement_id_list:
+                statement_id_full = 'Q' + str(statement_id)
 
-                leb = self.get_item_label(output1)
-                dis = self.get_item_descriptions(output1)
-                aka_list = self.get_item_aliases(output1)
+                leb = self.get_item_label(statement_id_full)
+                dis = self.get_item_descriptions(statement_id_full)
+                aka_list = self.get_item_aliases(statement_id_full)
 
                 if self.is_in_text(leb, value):
-                    return output1
+                    return statement_id_full
                 for aka in aka_list:
                     if self.is_in_text(aka, value):
-                        return output1
+                        return statement_id_full
                 if self.is_in_text(dis, value):
-                    return output1
+                    return statement_id_full
         else:
-            print("None")
+            logging.info("Value in property id not found, or property id not found")
 
     # used for method in text finding
+    # need to be better done !!!
     def preprocess_text(self, text):
         text = text.lower().strip()
-        text = re.sub('[^A-Za-z0-9 ]+', '', text)
+        text = re.sub("[^A-Za-z0-9 ]+", '', text)
         return text
 
     # checks if is RexEx in the text
     def is_in_text(self, text_to_find, original_text):
+
         original_text = self.preprocess_text(original_text)
         text_to_find = self.preprocess_text(text_to_find)
-        print("1"+original_text)
-        print("2"+text_to_find)
+
         x = re.findall(text_to_find, original_text)
-        print(original_text)
-        print(text_to_find==original_text)
         if x:
             return True
         return False
 
     def search_info_by_property(self, item_id, property_id, value):
         if value != '':
+            logging.info(f"Searching for value: {value} | by property id: {property_id} | in item: {item_id} ")
             return self.search_by_property(item_id, property_id, value)
         else:
             return ""
 
     def gene_search(self, text_list):
-        print("repositories are searching")
-        print(text_list)
-        resps = self.search_entities(text_list[0])
-        # print(json.dumps(resps, indent=2))
-        all_outputs = []
-        a = 0
-        for respon in resps["search"]:
-            a += 1
-            print(f"Blablabla: {a}")
-            protein = self.search_by_property(respon["id"], ENCODES, text_list[1])
+        found_entities = self.search_entities(text_list[0])
+        nr_found = 0
+        for found_item_id in found_entities["search"]:
+            nr_found += 1
+            logging.info(f"number of items found by searching: {nr_found} ")
+            protein = self.search_by_property(found_item_id["id"], ENCODES, text_list[1])
             if protein:
-                id_list = [respon["id"], protein]
-                # print("####################")
-                id_list.append(self.search_info_by_property(protein, BIOLOGICAL_PROCESS, text_list[2]))
+                id_list = [found_item_id["id"], protein,
+                           self.search_info_by_property(protein, BIOLOGICAL_PROCESS, text_list[2]),
+                           self.search_info_by_property(protein, MOLECULAR_FUNCTION, text_list[3]),
+                           self.search_info_by_property(protein, CELL_COMPONENT, text_list[4])]
 
-                # print("####################")
-                id_list.append(self.search_info_by_property(protein, MOLECULAR_FUNCTION, text_list[3]))
+                logging.info(f"List found: {id_list}")
 
-                # print("####################")
-                id_list.append(self.search_info_by_property(protein, CELL_COMPONENT, text_list[4]))
-                print(id_list)
-                all_outputs.append(id_list)
                 if None not in id_list:
                     return id_list
 
+        logging.info(f"Nothing found in items: {found_entities['search']}")
         return None
 
-    def try_different_cinfiguration(self, text_list):
-        print("try_different_cinfiguration",str(text_list))
+    # changing place first and second element
+    # has to be improved !!!
+    def try_different_configuration(self, text_list):
+        logging.info(f"try_different_configuration for: {text_list}")
         text_list1 = text_list
-        text_list1[0], text_list1[1] = text_list1[1], text_list1[0]
+        text_list1[0:1] = text_list1[1:0:-1]
         item_id_list = self.gene_search(text_list1)
         if item_id_list:
             return item_id_list
 
-    def try_split_values(self, text_list):
-        print("try_split_values", str(text_list))
+    def try_by_splitting_values(self, text_list):
+        logging.info(f"trying to find some values by splitting: {text_list}")
         for new_item in reversed(text_list[0].split()):
-            text_list1 = text_list
-            text_list1[0] = new_item
-            item_id_list = self.gene_search(text_list1)
+            text_list_new = text_list
+            text_list_new[0] = new_item
+            item_id_list = self.gene_search(text_list_new)
             if item_id_list:
                 return item_id_list
 
-    def start_process(self, text_list):
+    def start(self, text_list):
+        logging.info("Starting searching first item")
         item_id_list = self.gene_search(text_list)
-        if not item_id_list:            # if found nothing splitting first value of row to separate words
+        if not item_id_list:  # if found nothing splitting first value of row to separate words
 
-            item_id_list = self.try_different_cinfiguration(text_list)
+            item_id_list = self.try_different_configuration(text_list)
             if item_id_list:
+                logging.info("Found list of items with success!")
                 return item_id_list
 
-            item_id_list = self.try_split_values(text_list)
+            item_id_list = self.try_by_splitting_values(text_list)
             if item_id_list:
+                logging.info("Found list of items with success!")
                 return item_id_list
         else:
+            logging.info("Found list of items with success!")
             return item_id_list
 
-        print("Finished: found nothing")
+        print("Finished: nothing found ")
+        logging.info("Finished: nothing found ")
         return None
 
 
 if __name__ == "__main__":
     data = ["Hypothetical protein CELE_Y116A8B.1", "Y116A8B.1", "", "", ""]
     find_wiki = FindWikiPage()
-    end = find_wiki.start_process(data)
+    id_found = find_wiki.start(data)
+    print(f"Ids that were found: {id_found} ")
